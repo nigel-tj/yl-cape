3d466d0a312a68861cc7c333bd31b69656d1146c
nigel_tj <hypej.10@gmail.com>
2015-05-01 18:05:51 +0000

    the navbar fix

--- app/models/video_upload.rb
+++ app/models/video_upload.rb
@@ -1,9 +1,11 @@
 class VideoUpload < ActiveType::Object
-  attribute :description, :text
   attribute :file, :varchar
   attribute :title, :varchar
+  attribute :description, :text
+
   validates :file, presence: true
   validates :title, presence: true
+
   def upload!(user)
     account = Yt::Account.new access_token: user.token
     account.upload_video self.file, title: self.title, description: self.description
