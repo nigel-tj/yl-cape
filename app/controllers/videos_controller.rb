480b3693346fe301e6177ae5c9b87cdd7c7a89f2
nigel_tj <hypej.10@gmail.com>
2015-05-01 17:10:25 +0000

    the navbar fix

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -8,5 +8,10 @@ class VideosController < ApplicationController
   end
   
   def create
+    if params[:file].present? && params[:title].present? # ... and more checks here
+      # upload video
+    else
+      # display an error (and user won't even understand what exactly is wrong)
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users
-  resources :videos, only: [:index, :new, :create]  
+  resources :videos, only: [:index, :new, :create]
+  resources :video_uploads, only: [:new, :create]
   get '/auth/:provider/callback', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy', as: :logout
   root to: 'videos#index'
