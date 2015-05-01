69631369147a49d5f477cc15b2272ae91176cdb2
nigel_tj <hypej.10@gmail.com>
2015-05-01 17:54:30 +0000

    the home page redisgn

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -2,48 +2,24 @@ class VideosController < ApplicationController
   def index
     @videos = Video.order('created_at DESC')
   end
-  
+
   def new
     @video = Video.new
   end
 
-def create
-    @video_upload = VideoUpload.new(title: params[:video_upload][:title], description: params[:video_upload][:description], file: params[:video_upload][:file].try(:tempfile).try(:to_path))
-
-    if @video_upload.save
-          uploaded_video = @video_upload.upload!(current_user)
-
-          if uploaded_video.failed?
-                  flash[:error] = 'There was an error while uploading your video...'
-                else
-                  Video.create({link: "https://www.youtube.com/watch?v=#{uploaded_video.id}"})
-                  flash[:success] = 'Your video has been uploaded!'
-                end
-          redirect_to root_url
-        else
-          render :new
-        end
+  def create
+    @video = Video.new(video_params)
+    if @video.save
+      flash[:success] = 'Video added!'
+      redirect_to root_url
+    else
+      render :new
+    end
   end
 
-# def create
-#     @video_upload = VideoUpload.new(title: params[:video_upload][:title],
-#                                                                       description: params[:video_upload][:description],
-#                                                                       file: params[:video_upload][:file].try(:tempfile).try(:to_path))
-#     if @video_upload.save
-#           uploaded_video = @video_upload.upload!(current_user)
+  private
 
-#           # check if the video was uploaded or not
-
-#           redirect_to root_url
-#         else
-#           render :new
-#         end
-#   end
-  # def create
-  #   if params[:file].present? && params[:title].present? # ... and more checks here
-  #     # upload video
-  #   else
-  #     # display an error (and user won't even understand what exactly is wrong)
-  #   end
-  # end
+  def video_params
+    params.require(:video).permit(:link)
+  end
 end
