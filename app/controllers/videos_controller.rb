79fc7ac8a8575def54f62825fc9467e5a4d0d022
nigel_tj <hypej.10@gmail.com>
2015-05-01 17:25:51 +0000

    youtube

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -6,12 +6,44 @@ class VideosController < ApplicationController
   def new
     @video = Video.new
   end
-  
-  def create
-    if params[:file].present? && params[:title].present? # ... and more checks here
-      # upload video
-    else
-      # display an error (and user won't even understand what exactly is wrong)
-    end
+
+def create
+    @video_upload = VideoUpload.new(title: params[:video_upload][:title], description: params[:video_upload][:description], file: params[:video_upload][:file].try(:tempfile).try(:to_path))
+
+    if @video_upload.save
+          uploaded_video = @video_upload.upload!(current_user)
+
+          if uploaded_video.failed?
+                  flash[:error] = 'There was an error while uploading your video...'
+                else
+                  Video.create({link: "https://www.youtube.com/watch?v=#{uploaded_video.id}"})
+                  flash[:success] = 'Your video has been uploaded!'
+                end
+          redirect_to root_url
+        else
+          render :new
+        end
   end
+
+# def create
+#     @video_upload = VideoUpload.new(title: params[:video_upload][:title],
+#                                                                       description: params[:video_upload][:description],
+#                                                                       file: params[:video_upload][:file].try(:tempfile).try(:to_path))
+#     if @video_upload.save
+#           uploaded_video = @video_upload.upload!(current_user)
+
+#           # check if the video was uploaded or not
+
+#           redirect_to root_url
+#         else
+#           render :new
+#         end
+#   end
+  # def create
+  #   if params[:file].present? && params[:title].present? # ... and more checks here
+  #     # upload video
+  #   else
+  #     # display an error (and user won't even understand what exactly is wrong)
+  #   end
+  # end
 end
--- app/models/video_upload.rb
+++ app/models/video_upload.rb
@@ -4,4 +4,8 @@ class VideoUpload < ActiveType::Object
   attribute :title, :varchar
   validates :file, presence: true
   validates :title, presence: true
+  def upload!(user)
+    account = Yt::Account.new access_token: user.token
+    account.upload_video self.file, title: self.title, description: self.description
+  end
 end
