48a548b8e30333977b9acf17979a57a5ea3df6d7
nigel_tj <hypej.10@gmail.com>
2015-05-01 17:01:27 +0000

    the push up to heroku master

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   resources :videos, only: [:index, :new, :create]  
   get '/auth/:provider/callback', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy', as: :logout
-  root to: 'visitors#index'
+  root to: 'videos#index'
 end
