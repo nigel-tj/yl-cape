8c12ed06139624774a147044297f8192ef6b600a
nigel_tj <hypej.10@gmail.com>
2015-05-01 18:00:54 +0000

    the push up online

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users
-  resources :videos, only: [:index, :new, :create]  
+  resources :videos, only: [:index, :new, :create]
+  resources :video_uploads, only: [:new, :create]
   get '/auth/:provider/callback', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy', as: :logout
-  root to: 'visitors#index'
+
+  root to: 'vistors#index'
 end
