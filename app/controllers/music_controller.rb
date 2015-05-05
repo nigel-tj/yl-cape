0b473291cd6d704ef9dec857e4d8b18efbbeffe0
nigel_tj <hypej.10@gmail.com>
2015-05-05 10:35:41 +0000

    the sound clould addition

--- app/controllers/music_controller.rb
+++ app/controllers/music_controller.rb
@@ -1,7 +1,7 @@
 require 'soundcloud'
 class MusicController < ApplicationController
   def index
-    client = Soundcloud.new(:client_id => '3239f804ddc1933e6f45a436c6d2f0aa')
-    @tracks = client.get('/tracks' , :limit => 10 , :order => 'hotness')
+    @client = Soundcloud.new(:client_id => '3239f804ddc1933e6f45a436c6d2f0aa',:client_secret => 'baeaa699992bdf7a3283a602b6f7ee8d')
+    
   end
 end
