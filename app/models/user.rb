d258fe025290f81e4828db9542f55cf730dba51b
nigel_tj <hypej.10@gmail.com>
2015-05-07 14:15:59 +0000

    adding of the admin feature

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ActiveRecord::Base
   after_initialize :set_default_role, :if => :new_record?
 
   def set_default_role
-    self.role ||= :user
+    self.role ||= :admin
   end
   class << self
     def from_omniauth(auth)
