b05f971a5dec0829d8294df14ce67319bdc22716
nigel_tj <hypej.10@gmail.com>
2015-05-21 18:19:05 +0000

    the final push before the break

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,6 +1,7 @@
 class VisitorsController < ApplicationController
     def index
         # @tweets = $twitter.user_timeline("Redcupvillage") 
+        @videos = Video.order('created_at DESC')
       
     end
 end
