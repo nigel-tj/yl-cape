3c435eb65ce919f25cea9fbba3ec0b7ac3758e06
nigel_tj <hypej.10@gmail.com>
2015-05-21 23:23:14 +0000

    the push up towards heroku

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,6 +1,6 @@
 class VisitorsController < ApplicationController
     def index
-        @tweets = $twitter.search("SA Hip Hop", result_type: "recent")
+        @tweets = $twitter.search("SA Hip Hop", result_type: "recent").take(20)
         @videos = Video.order('created_at DESC')
       
     end
