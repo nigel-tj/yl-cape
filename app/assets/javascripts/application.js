7961636683d45941b261ad0ef1862e3a61f56bef
nigel_tj <hypej.10@gmail.com>
2015-05-22 22:24:27 +0000

    the push to heroku master

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,17 +13,9 @@
 //= require jquery
 //= require scripts
 //= require pongstagr.am
+//= require gallery
 //= require jquery_ujs
 //= require bootstrap-sprockets
 
-$('#selector').pongstgrm({ show: 'recent' });
-$(document).ready(function () {
- 
-  $('#selector').pongstgrm({
-    accessId:     '2f60e1a62f3047bfb5c6361fa47600c1',
-    accessToken:  '1052563623.1fb234f.99cb9eff244d48e8a71027ed63f6830f'
-  });
- 
-});
 
 //= require_tree .
