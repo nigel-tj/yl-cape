6fc163968c38149b8f395b770d88849ba39801a6
nigel_tj <hypej.10@gmail.com>
2015-05-22 21:51:42 +0000

    the fix on the front end

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,6 +1,6 @@
 class VisitorsController < ApplicationController
     def index
-        @tweets = $twitter.search("hip hop news", result_type: "recent").take(20)
+        @tweets = $twitter.search("hip hop news", result_type: "recent").take(10)
         @videos = Video.order('created_at DESC')
       
     end
