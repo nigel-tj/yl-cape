a68f38a28fe36c53e286ff7bb3debd5c5cda4131
nigel_tj <hypej.10@gmail.com>
2015-05-22 00:17:54 +0000

    the change in the search

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,6 +1,6 @@
 class VisitorsController < ApplicationController
     def index
-        @tweets = $twitter.search("SA Hip Hop", result_type: "recent").take(20)
+        @tweets = $twitter.search("Africa Urban Street news", result_type: "recent").take(20)
         @videos = Video.order('created_at DESC')
       
     end
