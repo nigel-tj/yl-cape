a149bfefd07c35992c8de65e57e29c630fda83e0
nigel_tj <hypej.10@gmail.com>
2015-05-24 02:03:37 +0000

    the push for videos

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,23 +20,23 @@
 //= require_tree .
 jQuery(document).ready(function($) {
  
-        $('#Video-Carousel').carousel({
+        $('#video-carousel').carousel({
                 interval: 5000
         });
  
         //Handles the carousel thumbnails
-        $('[id^=carousel-selector-]').click(function () {
+        $('[id^=#_]').click(function () {
         var id_selector = $(this).attr("id");
         try {
             var id = /-(\d+)$/.exec(id_selector)[1];
             console.log(id_selector, id);
-            jQuery('#Video-Carousel').carousel(parseInt(id));
+            jQuery('#video-carousel').carousel(parseInt(id));
         } catch (e) {
             console.log('Regex failed!', e);
         }
     });
         // When the carousel slides, auto update the text
-        $('#Video-Carousel').on('slid.bs.carousel', function (e) {
+        $('#video-carousel').on('slid.bs.carousel', function (e) {
                  var id = $('.video-item.active').data('slide-number');
                 $('#carousel-text').html($('#slide-content-'+id).html());
         });
