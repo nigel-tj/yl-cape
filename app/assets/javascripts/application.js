fd03e2293d3ef2d24b6862f2813e944bf14e4940
nigel_tj <hypej.10@gmail.com>
2015-05-24 00:52:46 +0000

    the video stylying

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,3 +18,26 @@
 
 
 //= require_tree .
+jQuery(document).ready(function($) {
+ 
+        $('#Video-Carousel').carousel({
+                interval: 5000
+        });
+ 
+        //Handles the carousel thumbnails
+        $('[id^=carousel-selector-]').click(function () {
+        var id_selector = $(this).attr("id");
+        try {
+            var id = /-(\d+)$/.exec(id_selector)[1];
+            console.log(id_selector, id);
+            jQuery('#Video-Carousel').carousel(parseInt(id));
+        } catch (e) {
+            console.log('Regex failed!', e);
+        }
+    });
+        // When the carousel slides, auto update the text
+        $('#Video-Carousel').on('slid.bs.carousel', function (e) {
+                 var id = $('.video-item.active').data('slide-number');
+                $('#carousel-text').html($('#slide-content-'+id).html());
+        });
+});
