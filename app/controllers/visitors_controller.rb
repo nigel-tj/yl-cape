be2ce64a7352c1590d09a88268ca10f130cd3f56
nigel_tj <hypej.10@gmail.com>
2015-05-24 20:47:04 +0000

    the push

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,6 +1,6 @@
 class VisitorsController < ApplicationController
     def index
-        @tweets = $twitter.search("life style news", result_type: "recent")
+        @tweets = $twitter.search("life style news", result_type: "recent").take(100)
         
         @videos = Video.order('created_at DESC')
       
