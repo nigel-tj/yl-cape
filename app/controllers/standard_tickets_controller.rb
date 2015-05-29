11ed3b3fd314eb223dafd776742d35c069143c33
nigel_tj <hypej.10@gmail.com>
2015-05-29 23:02:46 +0000

    the fix on the create

--- app/controllers/standard_tickets_controller.rb
+++ app/controllers/standard_tickets_controller.rb
@@ -1,2 +1,19 @@
 class StandardTicketsController < ApplicationController
+     def create
+        if current_user 
+          @event = Event.find_by_id(params[:event_id])
+          
+            @event.standard_tickets.create({user_id: current_user.id})
+             flash[:notice] = "Ticket has been Sent to your facebook photo : "
+             redirect_to root_path    
+        else
+             flash[:success] = 'ticket has been booked already'
+            redirect_to events_path
+         
+          #flash[:warning] = 'you have liked this qwatos before'
+         # redirect_to qwatos_path
+          #render js:  'alert(\'Your like cannot be saved.\');'
+        end
+        
+      end
 end
