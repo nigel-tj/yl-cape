4d49aaa7bae15fe7a1e3a90fc52a3651f977b317
nigel_tj <hypej.10@gmail.com>
2015-06-25 08:04:18 +0000

    add routes for split pages

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,13 @@ Rails.application.routes.draw do
   get 'gallery/index'
   get "/callback" => "facebook#callback"
   get "/facebook_profile" => "facebook#facebook_profile"
+  get 'visitors/news'
+  get 'visitors/gallery'
+  get 'visitors/videos'
+  get 'visitors/social'
+  get 'visitors/events'
+  get 'visitors/contact'
+
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   resources :users
   resources :musics
@@ -23,6 +30,12 @@ Rails.application.routes.draw do
   
   get '/auth/:provider/callback', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy', as: :logout
+
+  match 'gallery' => 'visitors#gallery', :via => :get
+  match 'music' => 'visitors#music', :via => :get
+  match 'news' => 'visitors#news', :via => :get
+  match 'social' => 'visitors#social', :via => :get
+  match 'videos' => 'visitors#videos', :via => :get
 
   root to: 'visitors#index'
 end
