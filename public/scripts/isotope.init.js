7c06bc6b145b159dc16f929ebf0dae01ba531596
Nigel <hypej.10@gmail.com>
2016-04-14 17:57:49 +0200

    continue styling pages

---
+++ public/scripts/isotope.init.js
@@ -0,0 +1,27 @@
+  $(window).load(function(){
+    var $container = $('.grid');
+    $container.isotope({
+        filter: '*',
+        animationOptions: {
+            duration: 750,
+            easing: 'linear',
+            queue: false
+        }
+    });
+ 
+    $('.content_filter li').click(function(){
+        $('.portfolioFilter .current').removeClass('current');
+        $(this).addClass('current');
+ 
+        var selector = $(this).attr('data-filter');
+        $container.isotope({
+            filter: selector,
+            animationOptions: {
+                duration: 750,
+                easing: 'linear',
+                queue: false
+            }
+         });
+         return false;
+    }); 
+  });
