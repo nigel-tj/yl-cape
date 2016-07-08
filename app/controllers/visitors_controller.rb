8032c73aaa0dab975538f9d24016c21409fe84b1
nigel <hypej.10@gmail.com>
2016-07-08 15:41:55 +0200

    design team page

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,27 +1,31 @@
 require 'rqrcode_png'  
 class VisitorsController < ApplicationController
   #before_action :authenticate_user! #, except: [:index,:show, :vidoes, :social, :news, :portfolio,:gallery]
-    def index
-      @banners = MainBanner.all
-      @trends  = Feature.all
-      @videos = Video.order('created_at DESC')
-      # twiiter
+  layout "team_layout", only: [:team]
+  def index
+    @banners = MainBanner.all
+    @trends  = Feature.all
+    @videos = Video.order('created_at DESC')
+    # twiiter
         #@tweets = $twitter.search("UpperEchelon12June ", result_type: "recent").take(20)
-           
-      # @client = Instagram.client(:access_token  => '1052563623.1fb234f.99cb9eff244d48e8a71027ed63f6830f')
-      # default_search = @client.tag_search('Redcupvillage')
-      # @tag = default_search
-      # @tag = @tag.first.name
-      # @results = @client.tag_recent_media(@tag)
-      # @graph = Koala::Facebook::API.new( '427969987356450|1BAAiHW4nnWkIL7NfhsI7pi5dD0')
-      # @feed = @graph.get_connections("TheRedcupvillage", "posts",'type' => 'large')
-      # @qr = RQRCode::QRCode.new("http://redvillage.herokuapp.com").to_img.resize(200, 200).to_data_url
-        
-      
+    
+    # @client = Instagram.client(:access_token  => '1052563623.1fb234f.99cb9eff244d48e8a71027ed63f6830f')
+    # default_search = @client.tag_search('Redcupvillage')
+    # @tag = default_search
+    # @tag = @tag.first.name
+    # @results = @client.tag_recent_media(@tag)
+    # @graph = Koala::Facebook::API.new( '427969987356450|1BAAiHW4nnWkIL7NfhsI7pi5dD0')
+    # @feed = @graph.get_connections("TheRedcupvillage", "posts",'type' => 'large')
+    # @qr = RQRCode::QRCode.new("http://redvillage.herokuapp.com").to_img.resize(200, 200).to_data_url
+    
+    
     end
 
     def videos
        @videos = Video.order('created_at DESC')
+    end
+
+    def team
     end
 
     def social
