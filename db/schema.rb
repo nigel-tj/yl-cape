b5d328c23fdf74612e9d243fe1fba51c2ac62ef8
Nigel <hypej.10@gmail.com>
2018-01-04 16:06:23 +0200

    setup ecommerce

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161007095450) do
+ActiveRecord::Schema.define(version: 20180104140243) do
 
   create_table "admin_users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
